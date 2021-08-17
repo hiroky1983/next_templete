656ee520c137773fe413a4061a055c09f7e25894
yamadahiroki <hirockysan1983@gmail.com>
2021-08-17 20:01:02 +0900

    commit

---
+++ pages/auth.tsx
@@ -0,0 +1,8 @@
+export const Auth = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <h1>ログイン</h1>
+          <button>ログアウト</button>
+        </div>
+      </main>
+    </div>
+  );
+}
