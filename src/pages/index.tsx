537eb248a3a1240ba38059e5ba24c53a880200c0
yamadahiroki <hirockysan1983@gmail.com>
2022-01-12 22:19:11 +0900

    ⚡️テンプレートの更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
+import { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
-export default function Home() {
+export const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
