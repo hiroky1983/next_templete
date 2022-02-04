52522398491532fb7867668426dbc6935526f597
yamadahiroki <hirockysan1983@gmail.com>
2022-02-04 23:19:53 +0900

    ✨ getStaticPropsの練習

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from "next";
 import Head from "next/head";
 
-export const Home: NextPage = () => {
+type Props = {
+  [key: string]: any;
+};
+
+const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -9,8 +13,20 @@ export const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+        <ul>
+          {posts.map((post: any) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ul>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const posts = await res.json();
+  return { props: { posts } };
+};
