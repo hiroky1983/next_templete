7a9bd25327e993413fa03e70ba02aa4eb2845949
yamadahiroki <hirockysan1983@gmail.com>
2022-03-24 00:49:33 +0900

    ✨ テンプレート更新

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <h1>Next template</h1>
       </main>
