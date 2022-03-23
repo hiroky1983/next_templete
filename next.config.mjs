c16ae0500c0a874159de3c4f3d29192a914b1085
yamadahiroki <hirockysan1983@gmail.com>
2022-03-24 00:53:47 +0900

    ♻️ 不要なコード削除

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,3 @@
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-
-if (!SUPABASE_URL) {
-  throw new Error("環境変数が未定義 : env.NEXT_PUBLIC_SUPABASE_URL");
-}
-
-const DOMAIN = SUPABASE_URL.split("/").slice(-1)[0];
-
 /**
  * @type {import('next').NextConfig}
  **/
